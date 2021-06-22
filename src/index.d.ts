06460b11e27ac344e8a8919fb9cf7fb515b74f2b
vedansh2019 <shekharkumar.gh@gmail.com>
2021-06-22 17:11:59 +0530

    add option for play and pause slider

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,6 +3,8 @@ import Vue from "vue";
 declare class Slick extends Vue {
     next(): void;
     prev(): void;
+    play(): void;
+    pause(): void;
     reSlick(): void;
     goTo(index: number): void;
 }
